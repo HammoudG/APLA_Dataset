c49c3544fd3e6e301243d0fc44ebd5aaf5f23315
Omar HG <61126529+HammoudG@users.noreply.github.com>
2020-11-17 17:27:04 +0000

    Add files via upload
    Pushed all changes

--- data-raw/build.R
+++ data-raw/build.R
@@ -26,36 +26,14 @@ library(readxl)
 library(readr)
 
 
-Data_Refugees_UNHCR <- read.csv("~/R-projects/APLA_DatasetHammoud/data-raw/Data_Refugees_UNHCR.csv")
+Data_Refugees_UNHCR <- read.csv("C:/Users/omarh/Desktop/Git_APLA/APLA_Dataset/data-raw/Data_Refugees_UNHCR.csv")
 save(Data_Refugees_UNHCR, file =  "data/Data_Refugees_UNHCR.RData")
 sinew::makeOxygen(Data_Refugees_UNHCR, add_fields = "source")
 
-APLA_Database <- read_excel("data-raw/APLA Database.xlsx")
+APLA_Database <- read.csv("data-raw/APLA_Database.csv")
 save(APLA_Database, file =  "data/APLA_Database.RData")
 sinew::makeOxygen(APLA_Database, add_fields = "source")
 
-
-APLA_Regulatory_Complexity <- read_excel("data-raw/APLA Regulatory Complexity.xlsx",
-                                         sheet = "Tabelle1")
-save(APLA_Regulatory_Complexity, file =  "data/APLA_Regulatory_Complexity.RData")
-sinew::makeOxygen(APLA_Regulatory_Complexity, add_fields = "source")
-
-
-APLA_Liberalisation <- read_excel("data-raw/APLA Liberalisation.xlsx",
-                                  sheet = "Tabelle1")
-save(APLA_Liberalisation, file =  "data/APLA_Liberalisation.RData")
-sinew::makeOxygen(APLA_Liberalisation, add_fields = "source")
-
-Data_APLA <- read_csv("data-raw/Data_APLA.csv")
-save(Data_APLA, file =  "data/Data_APLA.RData")
-sinew::makeOxygen(Data_APLA, add_fields = "source")
-
-
-Data_APLA_1 <- read_csv("data-raw/Data_APLA_1.csv")
-save(Data_APLA_1, file =  "data/Data_APLA_1.RData")
-sinew::makeOxygen(Data_APLA_1, add_fields = "source")
-
-
 APLA_Map <- read.csv("data-raw/Data_APLA_Maps.csv")
 save(APLA_Map, file =  "data/APLA_Map.RData")
-sinew::makeOxygen(APLA_Map, add_fields = "source")
+sinew::makeOxygen(APLA_Map, add_fields = "source")=
